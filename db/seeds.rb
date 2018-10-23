f8bb19cddb9fb5242b3a0b29efbd10e1ff86834d
danielgarda <danielgarda@gmail.com>
2018-10-23 16:45:02 -0300

    ultimo commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+12.times do |i|
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "http://lorempixel.com/400/200/technics/#{i + 1}/" 
+  )
+end
